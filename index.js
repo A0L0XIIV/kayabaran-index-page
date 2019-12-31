74bbb6c67e43dd61fe8078d38aea5917d64846d3
A0L0XIIV <brnky_95@hotmail.com>
2019-12-31 15:40:30 -0500

    Language and dark buttons adjusted

--- index.js
+++ index.js
@@ -1,79 +1,11 @@
-function activateDarkMode() {
-  //   /* Add dark-body class to body tag */
-  //   document.getElementsByTagName("body").className += "dark-body";
-  //   /* Add dark-main class to main tag */
-  //   document.getElementsByTagName("main").className += "dark-main";
-  //   /* Add dark-content class to content class */
-  //   document.getElementsByClassName("content").className += "dark-content";
-
-  /* Add dark-body class to body tag */
-  document.getElementsByTagName("body")[0].classList.add("dark-body");
-  /* Add dark-main class to main tag */
-  document.getElementsByTagName("main")[0].classList.add("dark-main");
-  /* Add dark-content class to content class */
-  document.getElementsByClassName("content")[0].classList.add("dark-content");
-}
-
-function deactivateDarkMode() {
-  // document.getElementsByTagName(
-  //   "body"
-  // )[0].className = document
-  //   .getElementsByTagName("body")[0]
-  //   .className.replace(/(?:^|\s)dark-body(?!\S)/g, "");
-
-  // /* Remove dark-main class from main tag */
-  // document.getElementsByTagName(
-  //   "main"
-  // )[0].className = document
-  //   .getElementsByTagName("main")[0]
-  //   .className.replace(/(?:^|\s)dark-main(?!\S)/g, "");
-
-  // /* Remove dark-content class from content class */
-  // document.getElementsByClassName(
-  //   "content"
-  // )[0].className = document
-  //   .getElementsByClassName("content")[0]
-  //   .className.replace(/(?:^|\s)dark-content(?!\S)/g, "");
-
-  /* Remove dark-body class from body tag */
-  document.getElementsByTagName("body")[0].classList.remove("dark-body");
-  /* Remove dark-main class from main tag */
-  document.getElementsByTagName("main")[0].classList.remove("dark-main");
-  /* Remove dark-content class from content class */
-  document
-    .getElementsByClassName("content")[0]
-    .classList.remove("dark-content");
-}
-
 function switchDarkTheme() {
+  // Toggle class adds or removes the class depending on the  class's presence
   /* Swicht body theme*/
-  if (
-    document.getElementsByTagName("body")[0].className.includes("dark-body")
-  ) {
-    document.getElementsByTagName("body")[0].classList.remove("dark-body");
-  } else {
-    document.getElementsByTagName("body")[0].classList.add("dark-body");
-  }
-
+  $("body").toggleClass("dark-body");
   /* Swicht main div theme*/
-  if (
-    document.getElementsByTagName("main")[0].className.includes("dark-main")
-  ) {
-    document.getElementsByTagName("main")[0].classList.remove("dark-main");
-  } else {
-    document.getElementsByTagName("main")[0].classList.add("dark-main");
-  }
-
+  $("main").toggleClass("dark-main");
   /* Swicht content div theme*/
-  if (
-    document
-      .getElementsByClassName("content")[0]
-      .className.includes("dark-content")
-  ) {
-    document
-      .getElementsByClassName("content")[0]
-      .classList.remove("dark-content");
-  } else {
-    document.getElementsByClassName("content")[0].classList.add("dark-content");
-  }
+  $(".content").toggleClass("dark-content");
+  /* Swicht modal-content div theme*/
+  $(".modal-content").toggleClass("dark-modal");
 }
