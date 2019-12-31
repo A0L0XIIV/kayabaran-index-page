98099fd0c6c37d276851df3ade21b143a1c97ab6
A0L0XIIV <brnky_95@hotmail.com>
2019-12-31 00:09:50 -0500

    Dark-Theme fixed

--- index.js
+++ index.js
@@ -7,32 +7,40 @@ function activateDarkMode() {
   //   document.getElementsByClassName("content").className += "dark-content";
 
   /* Add dark-body class to body tag */
-  document.getElementsByTagName("body").classList.add("dark-body");
+  document.getElementsByTagName("body")[0].classList.add("dark-body");
   /* Add dark-main class to main tag */
-  document.getElementsByTagName("main").classList.add("dark-main");
+  document.getElementsByTagName("main")[0].classList.add("dark-main");
   /* Add dark-content class to content class */
-  document.getElementsByClassName("content").classList.add("dark-content");
+  document.getElementsByClassName("content")[0].classList.add("dark-content");
 }
 
 function deactivateDarkMode() {
+  // document.getElementsByTagName(
+  //   "body"
+  // )[0].className = document
+  //   .getElementsByTagName("body")[0]
+  //   .className.replace(/(?:^|\s)dark-body(?!\S)/g, "");
+
+  // /* Remove dark-main class from main tag */
+  // document.getElementsByTagName(
+  //   "main"
+  // )[0].className = document
+  //   .getElementsByTagName("main")[0]
+  //   .className.replace(/(?:^|\s)dark-main(?!\S)/g, "");
+
+  // /* Remove dark-content class from content class */
+  // document.getElementsByClassName(
+  //   "content"
+  // )[0].className = document
+  //   .getElementsByClassName("content")[0]
+  //   .className.replace(/(?:^|\s)dark-content(?!\S)/g, "");
+
   /* Remove dark-body class from body tag */
-  document.getElementsByTagName(
-    "body"
-  ).className = document
-    .getElementsByTagName("body")
-    .className.replace(/(?:^|\s)dark-body(?!\S)/g, "");
-
+  document.getElementsByTagName("body")[0].classList.remove("dark-body");
   /* Remove dark-main class from main tag */
-  document.getElementsByTagName(
-    "main"
-  ).className = document
-    .getElementsByTagName("main")
-    .className.replace(/(?:^|\s)dark-main(?!\S)/g, "");
-
+  document.getElementsByTagName("main")[0].classList.remove("dark-main");
   /* Remove dark-content class from content class */
-  document.getElementsByClassName(
-    "content"
-  ).className = document
-    .getElementsByClassName("content")
-    .className.replace(/(?:^|\s)dark-content(?!\S)/g, "");
+  document
+    .getElementsByClassName("content")[0]
+    .classList.remove("dark-content");
 }
